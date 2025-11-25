ddb2dd9bf339d4d46781f1c9b78c4e0c46379e5d
takyon22 <6cne@proton.me>
2025-11-25 13:56:18 +0100

    hehe

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let playerName = 'Axel';
+
+let numberBottles = 10;
+const line1 = 'green bottles hanging on the wall.';
+const line2 = 'And if one green bottle, should accidentally fall, there will be ';
+
+console.log(numberBottles + " " + line1 + " " + numberBottles + " " + line1);
+console.log(line2 + --numberBottles + " " + line1);
+
+//document.getElementById("output").innerHTML =
+  //"<p>Hej, <strong>" + playerName + "</strong>.</p>";
