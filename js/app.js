86579f00eacb3211f4947a1df182e4e81e959a36
takyon22 <6cne@proton.me>
2025-11-28 14:41:53 +0100

    cookie clicker!

--- js/app.js
+++ js/app.js
@@ -1,17 +1,33 @@
 
 const h1 = document.getElementById("h11");
-const para1 = document.getElementById("para1");
-const main = document.getElementById("wrapper");
+const scoreshow = document.getElementById("score");
 const button = document.getElementById("button");
 
+let score = 0;
 
 
+button.addEventListener("click", buttonClick);
 
+function buttonClick() {
 
+  score++;
 
-h1.innerText = "Wagwan rude bwoi!";
-
-para1.innerText = "Bombaclarttt...";
-para1.style.color = "#FE6BB3";
-
-
+  if (score > 39) {
+    scoreshow.innerText = "Score: " + score + "! Ehhhh....?";
+    scoreshow.style.color = "#ff5454";
+    scoreshow.style.fontweight = "1000";
+  } else if (score > 29) {
+    scoreshow.innerText = "Score: " + score + "! OMFG!!!!";
+    scoreshow.style.color = "#f56dfa";
+    scoreshow.style.fontweight = "800";
+  } else if (score > 19) {
+    scoreshow.innerText = "Score: " + score + "! Du e galen!";
+    scoreshow.style.color = "#8754FF";
+    scoreshow.style.fontweight = "bold";
+  } else if (score > 9) {
+    scoreshow.innerText = "Score: " + score + "! Wow! Fortsätt!";
+    scoreshow.style.fontweight = "bold";
+  } else {
+    scoreshow.innerText = "Score: " + score;
+  }
+}
