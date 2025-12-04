ac2ce61e9cd0c1910918156cd57b503fee5a8e6b
takyon22 <6cne@proton.me>
2025-12-04 09:58:27 +0100

    added matching color dropshadow for score button

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const scoreShow = document.getElementById("score");
 const button = document.getElementById("button");
-const headerText = document.getElementById("headertext");
+const headerText = document.getElementById("headerText");
 
 let score = 0;
 
@@ -13,17 +13,35 @@ function buttonClick() {
     scoreShow.innerText = score;
     scoreShow.style.color = "#ff5454";
     headerText.innerText = "Cookie Pro 😳";
-    scoreShow.style.fontWeight = "bold";
+    headerText.style.fontWeight = "500";
+    scoreShow.style.fontWeight = "500";
+    scoreShow.style.boxShadow = "0px 0px 30px #ff5454"
   } else if (score > 29) {
     scoreShow.innerText = score;
     scoreShow.style.color = "#f56dfa";
     headerText.innerText = "Cookie Enthusiast 😎";
+    headerText.style.fontWeight = "400";
+    scoreShow.style.fontWeight = "400";
+    scoreShow.style.boxShadow = "0px 0px 30px #f56dfa";
   } else if (score > 19) {
     scoreShow.innerText = score;
+    scoreShow.style.color = "#5283ff";
     headerText.innerText = "Cookie Rookie 😍";
+    headerText.style.fontWeight = "300";
+    scoreShow.style.fontWeight = "300";
+    scoreShow.style.boxShadow = "0px 0px 30px #5283ff";
+  } else if (score > 9) {
+    scoreShow.innerText = score;
+    scoreShow.style.color = "#83fa48";
+    headerText.innerText = "Cookie Rookie 😍";
+    headerText.style.fontWeight = "200";
+    scoreShow.style.fontWeight = "200";
+    scoreShow.style.boxShadow = "0px 0px 30px #83fa48";
   } else if (score > 0) {
     scoreShow.innerText = score;
     headerText.innerText = "Cookie Noob 💩";
+    headerText.style.fontWeight = "100";
+    scoreShow.style.fontWeight = "100";
   } else {
     scoreShow.innerText = score;
   }
